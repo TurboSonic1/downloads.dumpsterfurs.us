b8b3fedfbdf3ecf197d939120a20fd0700637447
TurboSonic1 <38984276+TurboSonic1@users.noreply.github.com>
2023-01-27 22:41:22 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to TurboSonic's download page" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          There's nothing here, yet... </code>
         </p>
       </main>
 
